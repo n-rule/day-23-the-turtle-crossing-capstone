959770460761ccb00d81175cc0569c342e3ebc3b
n-rule <rule@ukr.net>
2021-01-29 14:51:20 +0200

    1

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import time
 from turtle import Screen
+
 from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.move_forward(), "Up")
+screen.onkey(player.forward(5), "w")
+
+
+
 game_is_on = True
 while game_is_on:
+
     time.sleep(0.1)
     screen.update()
